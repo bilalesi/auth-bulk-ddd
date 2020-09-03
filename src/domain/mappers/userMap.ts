6b37daf1854a01fe66b59834a1f0b015f707377e
bilalesi <b_meddah@esi.dz>
2020-09-03 22:03:15 +0200

    add husky for git hooks

---
+++ src/domain/mappers/userMap.ts
@@ -0,0 +1,17 @@
+import User from "../duser/duser";
+
+
+class UserMap {
+    public static async toPersistence(user: User): Promise<any>{
+        
+        return{
+
+        }
+    }
+    public static toDto(user: User){
+
+    }
+    public static toDomain(user: User){
+
+    }
+}
